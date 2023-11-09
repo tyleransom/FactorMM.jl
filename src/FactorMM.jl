04b2f7e169d275e2d613e53bbcb08f689cddf8aa
Tyler Ransom <tyleransom@gmail.com>
2023-11-09 09:08:30 -0600

    initial test of CI and codecov and unit testing

--- src/FactorMM.jl
+++ src/FactorMM.jl
@@ -1,5 +1,11 @@
 module FactorMM
 
-# Write your package code here.
+using Distributions, Statistics
+
+function drawnorm(N = 100_000, μ = 1., σ = 2.)
+    return rand(Normal(μ, σ), N)
+end
+
+export drawnorm
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using FactorMM
 using Test
+using Statistics
 
-@testset "FactorMM.jl" begin
-    # Write your tests here.
+@testset "drawnorm checks" begin
+    outp = drawnorm()
+    @test size(outp) == (100_000,)
+    @test isapprox(mean(outp), 1., atol = 1E-2)
 end
